428a617756fe953c2def8bc17e6d5b2548613015
GreenCatty <dima.2004sh@list.ru>
2020-12-10 20:18:31 +0500

    Changes for the second task

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import random
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        size = random.randint(5, 300)
+        x = random.randint(0, 700)
+        y = random.randint(0, 200)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, size, size)
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
